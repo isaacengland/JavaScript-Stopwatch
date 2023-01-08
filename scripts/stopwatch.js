d51cae1585dd3ef182cca57522b0a4b928605b82
isaacengland <iengland368@gmail.com>
2023-01-08 10:03:46 -0600

    Fixed issues with the pad() function and timer width

--- scripts/stopwatch.js
+++ scripts/stopwatch.js
@@ -1,45 +1,82 @@
-const minutes = document.getElementsByClassName('minutes')[0];
-const seconds = document.getElementsByClassName('seconds')[0];
-const milliseconds = document.getElementsByClassName('milliseconds')[0];
+const minutesLabel = document.getElementsByClassName('minutes')[0];
+const secondsLabel = document.getElementsByClassName('seconds')[0];
+const millisecondsLabel = document.getElementsByClassName('milliseconds')[0];
+
+let minutes = 00;
+let seconds = 00;
+let milliseconds = 00;
 
 startBtn = document.getElementsByClassName('startBtn');
 resetBtn = document.getElementsByClassName('resetBtn');
 stopBtn = document.getElementsByClassName('stopBtn');
 
+let timerRunning = false;
+let interval;
+
+function pad(n) {
+	if (n < 10) {
+		return '0' + n;
+	} else {
+		return n;
+	}
+}
+
 function addMilliseconds() {
-	milliseconds.innerHTML++;
+	milliseconds++;
+	millisecondsLabel.innerHTML = pad(milliseconds);
 }
 
 function resetMilliseconds() {
-	milliseconds.innerHTML = '00';
+	milliseconds = 00;
+	millisecondsLabel.innerHTML = '00';
 }
 
 function addSeconds() {
-	seconds.innerHTML++;
+	seconds++;
+	secondsLabel.innerHTML = pad(seconds);
 }
 
 function resetSeconds() {
-	seconds.innerHTML = '00';
+	seconds = 00;
+	secondsLabel.innerHTML = '00';
 }
 
 function addMinutes() {
-	minutes.innerHTML++;
+	minutes++;
+	minutesLabel.innerHTML = pad(minutes);
 }
 
 function start() {
-	setInterval(function () {
-		if (milliseconds.innerHTML != 100) {
-			addMilliseconds();
-		} else {
-			resetMilliseconds();
-			if (seconds.innerHTML != 60) {
-				addSeconds();
+	clearInterval(interval);
+	timerRunning = true;
+	interval = setInterval(function () {
+		if (timerRunning) {
+			if (milliseconds != 99) {
+				addMilliseconds();
 			} else {
-				resetSeconds();
-				if (minutes.innerHTML != 60) {
-					addMinutes();
+				resetMilliseconds();
+				if (seconds != 59) {
+					addSeconds();
+				} else {
+					resetSeconds();
+					if (minutes != 59) {
+						addMinutes();
+					}
 				}
 			}
 		}
 	}, 1);
+}
+
+function stop() {
+	timerRunning = false;
+}
+
+function reset() {
+	milliseconds = 00;
+	seconds = 00;
+	minutes = 00;
+	millisecondsLabel.innerHTML = '00';
+	secondsLabel.innerHTML = '00';
+	minutesLabel.innerHTML = '00';
 }
